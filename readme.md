830fd1719736e03d849897d6f49aa111c6444333
irfan marzuki <irfnmzk@gmail.com>
2021-12-29 13:12:17 +0700

    first init

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Vite React Tailwindcss
+
+## Setup
+
+- yarn install && yarn dev
+- open http://localhost:3000
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <h1 className="text-xl text-red-400 bg-black">Hello Insignia</h1>
+    </Layout>
+  );
+};
+
+export default HomePage;
